777a16990bf732d66e209f3bec06af0f35c742e6
LennynVides <159490054+LennynVides@users.noreply.github.com>
2024-04-26 11:27:17 -0600

    Update apiIndex.js

--- js/apiIndex.js
+++ js/apiIndex.js
@@ -1,12 +1,10 @@
-let id = 1;
-
 async function obtenerNombreYImagenPokemon(id) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await response.json();
         const nombrePokemon = data.name;
         const imagenPokemon = data.sprites.front_default;
-        const tiposPokemon = data.types.map((tipo) => tipo.type.name);
+        const tiposPokemon = data.types.map((tipo) => tipo.type.name).join(', '); // Mostrar tipos separados por coma
         return { nombre: nombrePokemon, imagen: imagenPokemon, tipos: tiposPokemon };
     } catch (error) {
         console.error('Error al obtener los datos del Pokémon:', error);
@@ -17,15 +15,15 @@ async function mostrarNombreYImagenPokemon() {
     const pokemonNameContainer = document.getElementById('pokemonName');
     const pokemonTypeContainer = document.getElementById('pokemonType');
     const pokemonImageContainer = document.getElementById('pokemonImage');
+    const id = document.getElementById('pokemonNumberInput').value;
     const { nombre, imagen, tipos } = await obtenerNombreYImagenPokemon(id);
-    pokemonNameContainer.textContent = nombre;
-    pokemonTypeContainer.textContent = tipos;
+    pokemonNameContainer.textContent = `Nombre: ${nombre}`;
+    pokemonTypeContainer.textContent = `Tipos: ${tipos}`;
     pokemonImageContainer.src = imagen;
 }
 
-mostrarNombreYImagenPokemon();
-
-function buscarPokemon() {
-    id = document.getElementById('pokemonNumberInput').value;
+async function buscarPokemon() {
     mostrarNombreYImagenPokemon();
 }
+
+mostrarNombreYImagenPokemon();
